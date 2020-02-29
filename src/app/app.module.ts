8cf1cf5021114fa5930258c85dd41f5f36b575dc
Murilo Gil Felpeto <murilofelpeto@gmail.com>
2020-02-29 13:39:23 -0300

    Create components and services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,26 @@ import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ResendRegistrationTokenComponent } from './components/resend-registration-token/resend-registration-token.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HeaderComponent } from './shared/components/navigation/header/header.component';
+import { DeleteUserComponent } from './shared/components/modals/delete-user/delete-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    ResendRegistrationTokenComponent,
+    EditUserComponent,
+    ListUserComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    DeleteUserComponent
   ],
   imports: [
     BrowserModule,
